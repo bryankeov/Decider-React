43725a325948a454c21c739020c1a16846c8bbf9
Bryan <suprnewb@gmail.com>
2023-01-09 13:32:12 +1100

    Setup files

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+function Form() {
+  return(
+    <form>
+      <label>
+        <input type="text"></input>
+      </label>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div className="header">
+      <h1>The Decider</h1>
+      <p>Let me make the decision for you.</p>
+    </div>
+  );
+}
+
+export default Header;

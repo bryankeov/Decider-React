6c2bd540f9012a8efa0ab42dfd6b1a130b85b749
Bryan <suprnewb@gmail.com>
2023-01-09 16:26:27 +1100

    Setup Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,28 @@
+import React, {useState} from "react";
+
 function Form() {
+  const [choices, setChoices] = useState(["Choice 1", "Choice 2"])
+
+  function addNewChoice(e) {
+    e.preventDefault()
+    const index = choices.length + 1;
+    setChoices([
+      ...choices,
+      `Choice ${index}`
+    ])
+  }
+
   return(
     <form>
-      <label>
-        <input type="text"></input>
-      </label>
+      {choices.map((item, index) => {
+        return (
+          <label key={index + 1} id={index + 1}>
+            {item}
+            <input type="text" className="choices"></input>
+          </label>
+        );
+      })}
+      <button className="addChoice" onClick={addNewChoice}>+</button>
     </form>
   );
 }

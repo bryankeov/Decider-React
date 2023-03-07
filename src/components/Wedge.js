308f3e5ed5823f9b26029dcd5d4e7650ab01a210
Dara Bun <dara209@gmail.com>
2023-03-07 19:58:41 +1100

    Fix Wedge (most of it)

--- src/components/Wedge.js
+++ src/components/Wedge.js
@@ -4,18 +4,27 @@ import "./Wedge.css";
 function Wedge(props) {
   const choices = props.choices;
   let wedgeSize = 360 / choices.length;
-
+  console.log('wedgeSize', wedgeSize)
   return (
     <div className="wedge-container">
       {choices.map((choice, i) => {
+        let transform = `rotate(${i * wedgeSize}deg) skewY(${
+          wedgeSize - 90
+        }deg)`;
+
+        if (choices.length === 2) {
+          transform = `rotate(${i * wedgeSize}deg) translateY(50%)`;
+        }
+        else if (choices.length === 1) {
+          transform = `rotate(${i * wedgeSize}deg) translateY(50%) translateX(-50%)`;
+        }
+
         return (
           <div
             key={i}
             className="wedge"
             style={{
-              transform: `rotate(${i * wedgeSize}deg) skewY(${
-                wedgeSize - 90
-              }deg)`,
+              transform,
               backgroundColor:
                 "#" +
                 (0x1000000 + Math.random() * 0xffffff)
